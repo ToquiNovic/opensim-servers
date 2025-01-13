0098eff4eebdf953219f6712edd6f8c9bde6f29a
Julian-Chingal <mauriciochingal16@gmail.com>
2025-01-12 19:47:38 -0500

    Color

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import { PORT } from "./config/config";
 const port = PORT
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on \x1b[32mhttp://localhost:${port}\x1b[0m`);
 });
